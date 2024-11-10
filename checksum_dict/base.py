88c59ca95f43543faded93d41d8171c46f0ea9bb
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-10 12:09:48 -0400

    chore: run black on all PRs (#5)
    * chore: run black on all PRs
    
    * Update black.yaml
    
    * chore: `black .`
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- checksum_dict/base.py
+++ checksum_dict/base.py
@@ -1,4 +1,3 @@
-
 from typing import TYPE_CHECKING, Dict, Iterable, Tuple, TypeVar, Union
 
 from checksum_dict import exceptions
@@ -14,12 +13,14 @@ T = TypeVar("T")
 
 _SeedT = Union[Dict[AnyAddressOrContract, T], Iterable[Tuple[AnyAddressOrContract, T]]]
 
+
 class ChecksumAddressDict(Dict[EthAddressKey, T]):
     """
     A dict that maps addresses to objects.
     Will automatically checksum your provided address key when setting and getting values.
     If you pass in a `seed` dictionary, the keys will be checksummed and the values will be set.
     """
+
     def __init__(self, seed: _SeedT = None) -> None:
         super().__init__()
         self.__dict__ = self
@@ -28,10 +29,10 @@ class ChecksumAddressDict(Dict[EthAddressKey, T]):
         if isinstance(seed, Iterable):
             for key, value in seed:
                 self[key] = value
-    
+
     def __repr__(self) -> str:
         return f"ChecksumAddressDict({str(dict(self))})"
-    
+
     def __getitem__(self, key: AnyAddressOrContract) -> T:
         try:
             # It is ~700x faster to perform this check and then skip the checksum if we find a result for this key
@@ -39,7 +40,7 @@ class ChecksumAddressDict(Dict[EthAddressKey, T]):
         except KeyError:
             # NOTE: passing instead of checksumming here lets us keep a clean exc chain
             pass
-            
+
         try:
             return super().__getitem__(EthAddressKey(key))
         except KeyError as e:
@@ -51,13 +52,13 @@ class ChecksumAddressDict(Dict[EthAddressKey, T]):
             super().__setitem__(key, value)
         else:
             super().__setitem__(EthAddressKey(key), value)
-    
+
     def _getitem_nochecksum(self, key: EthAddressKey) -> T:
         """
         You can use this method in custom subclasses to bypass the checksum ONLY if you know its already been done at an earlier point in your code.
         """
         return super().__getitem__(key)
-    
+
     def _setitem_nochecksum(self, key: EthAddressKey, value: T) -> None:
         """
         You can use this method in custom subclasses to bypass the checksum ONLY if you know its already been done at an earlier point in your code.
