ffd4ef404ce582997e41057c9d0886a770c8787c
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-27 01:37:29 -0400

    fix: errs

--- checksum_dict/_checksum.pyx
+++ checksum_dict/_checksum.pyx
@@ -1,7 +1,7 @@
-cdef const str _MATCH_LOWER = "01234567"
+cdef str _MATCH_LOWER = "01234567"
 
-cpdef str cchecksum(str norm_address_no_0x, str address_hash_hex_no_0x):
-    checksum_address_no_0x = "".join(
+def cchecksum(str norm_address_no_0x, str address_hash_hex_no_0x) -> str:
+    cdef str checksum_address_no_0x = "".join(
         addr_char if hash_char in _MATCH_LOWER else addr_char.upper()
         for addr_char, hash_char in zip(norm_address_no_0x, address_hash_hex_no_0x)
     )
