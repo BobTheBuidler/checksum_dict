fb838617cd75554ebf368fb528190a04f3f132e7
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-28 21:19:45 -0400

    feat: cache helper methods for singleton meta

--- checksum_dict/singleton.py
+++ checksum_dict/singleton.py
@@ -10,40 +10,54 @@ _LocksDict = DefaultDict[AnyAddressOrContract, threading.Lock]
 
 
 class ChecksumAddressSingletonMeta(type, Generic[T]):
-    def __init__(cls, name: str, bases: Tuple[type, ...], namespace: Dict[str, Any]) -> None:
+    def __init__(self, name: str, bases: Tuple[type, ...], namespace: Dict[str, Any]) -> None:
         super().__init__(name, bases, namespace)
-        cls.__instances: ChecksumAddressDict[T] = ChecksumAddressDict()
-        cls.__locks: _LocksDict = defaultdict(threading.Lock)
-        cls.__locks_lock: threading.Lock = threading.Lock()
+        self.__instances: ChecksumAddressDict[T] = ChecksumAddressDict()
+        self.__locks: _LocksDict = defaultdict(threading.Lock)
+        self.__locks_lock: threading.Lock = threading.Lock()
 
-    def __call__(cls, address: AnyAddressOrContract, *args: Any, **kwargs: Any) -> T:  # type: ignore
+    def __call__(self, address: AnyAddressOrContract, *args: Any, **kwargs: Any) -> T:  # type: ignore
         address = str(address)
         try:
-            return cls.__instances[address]
+            return self.__instances[address]
         except exceptions.KeyError:
             pass  # NOTE: passing instead of proceeding lets helps us keep a clean exc chain
 
-        with cls.__get_address_lock(address):
+        with self.__get_address_lock(address):
             # Try to get the instance again, in case it was added while waiting for the lock
             try:
-                return cls.__instances[address]
+                return self.__instances[address]
             except exceptions.KeyError:
                 pass  # NOTE: passing instead of proceeding here lets us keep a clean exc chain
 
             instance = super().__call__(address, *args, **kwargs)
-            cls.__instances[address] = instance
-        cls.__delete_address_lock(address)
+            self.__instances[address] = instance
+        self.__delete_address_lock(address)
         return instance
 
-    def __get_address_lock(cls, address: AnyAddressOrContract) -> threading.Lock:
-        """Makes sure the singleton is actually a singleton."""
-        with cls.__locks_lock:
-            return cls.__locks[address]
+    def __getitem__(self, address: AnyAddressOrContract) -> T:
+        """Get the singleton instance for `address` from the cache."""
+        return self.__instances[str(address)]
 
-    def __delete_address_lock(cls, address: AnyAddressOrContract) -> None:
+    def __setitem__(self, address: AnyAddressOrContract, item: T) -> None:
+        """Set the singleton instance for `address` from the cache.
+        
+        You can use this if you need to implement non-standard init sequences.
+        """
+        return self.__instances[str(address)]
+        
+    def get_instance(self, address: AnyAddressOrContract) -> Optional[T]:
+        return self.__instances.get(str(address), None)
+
+    def __get_address_lock(self, address: AnyAddressOrContract) -> threading.Lock:
+        """Makes sure the singleton is actually a singleton."""
+        with self.__locks_lock:
+            return self.__locks[address]
+
+    def __delete_address_lock(self, address: AnyAddressOrContract) -> None:
         """No need to maintain locks for initialized addresses."""
-        with cls.__locks_lock:
+        with self.__locks_lock:
             try:
-                del cls.__locks[address]
+                del self.__locks[address]
             except KeyError:
                 pass
