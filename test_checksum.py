9288cd78f20ff56c7f1a0f52c7ccb618b9fd75f4
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-12-03 19:42:30 +0000

    chore: refactor

--- test_checksum.py
+++ test_checksum.py
@@ -9,21 +9,20 @@ def test_checksum():
     lower = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".upper()
     assert to_checksum_address(lower) == "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 
+benchmark_addresses = []
+for i in range(
+    100000000000000000000000000000000000000000, 100000000000000000000000000000000000100000
+):
+    address = hex(i)[2:]
+    address = "0x" + "0" * (40 - len(address)) + address
+    benchmark_addresses.append(address)
 
 def test_benchmark():
-    addresses = []
-    for i in range(
-        100000000000000000000000000000000000000000, 100000000000000000000000000000000000100000
-    ):
-        address = hex(i)[2:]
-        address = "0x" + "0" * (40 - len(address)) + address
-        addresses.append(address)
-
     start = time.time()
-    checksummed = [to_checksum_address(address) for address in addresses]
+    checksummed = [to_checksum_address(address) for address in benchmark_addresses]
     cython_duration = time.time() - start
     start = time.time()
-    python = [to_checksum_address_py(address) for address in addresses]
+    python = [to_checksum_address_py(address) for address in benchmark_addresses]
     python_duration = time.time() - start
     print(f"cython took {cython_duration}s")
     print(f"python took {python_duration}s")
