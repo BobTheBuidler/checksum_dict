bdde8cf8fe6c8fa82717f1f417a41fcd12cb8185
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-28 17:04:45 -0400

    Update _utils.py

--- checksum_dict/_utils.py
+++ checksum_dict/_utils.py
@@ -4,19 +4,26 @@ The following code was ripped out of eth-brownie on 2022-Aug-06.
 A big thanks to the many maintainers and contributors for their valuable work!
 """
 
-from typing import Final, Union
+from typing import TYPE_CHECKING, Final, Union
 
 import cchecksum  # type: ignore [import-not-found]
 from eth_typing import ChecksumAddress  # type: ignore [import-not-found]
 
+if TYPE_CHECKING:
+    from brownie import Contract
 
+
+# must not be Final so it can be redefined with lru cache in ypricemagic
 to_checksum_address = cchecksum.to_checksum_address
 
 
-def attempt_checksum(value: Union[str, bytes]) -> ChecksumAddress:
+def attempt_checksum(value: Union[str, bytes, Contract]) -> ChecksumAddress:
     # sourcery skip: merge-duplicate-blocks
     if isinstance(value, str):
         return checksum_or_raise(value)
+    elif isinstance(value, Contract):
+        # already checksummed
+        return value.address
     elif type(value) is bytes:  # only actual bytes type, mypyc will optimize this
         return checksum_or_raise(value.hex())
     else:  # other bytes types, mypyc will not optimize this
