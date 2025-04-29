07d01139fdb95972b539ad7cc1ef3481bf096c48
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-29 04:41:51 -0400

    Update _typing.py

--- checksum_dict/_typing.py
+++ checksum_dict/_typing.py
@@ -6,26 +6,22 @@ from typing_extensions import runtime_checkable
 
 # We need to assign mypyc has issues compiling TYPE_CHECKING
 # TODO: PR to mypyc to fix this
-
-
-class _AddressHaver(Protocol):
-    address: ChecksumAddress
-
-
 if TYPE_CHECKING:
     import brownie  # type: ignore [import-not-found]
     import y  # type: ignore [import-not-found]
 
-    Contract = Union[brownie.Contract, y.Contract]
+    Contract = brownie.Contract
     ERC20 = y.ERC20
 
 else:
 
     @runtime_checkable
-    class Contract(_AddressHaver): ...
+    class Contract(_AddressHaver):
+        address: ChecksumAddress
 
     @runtime_checkable
-    class ERC20(_AddressHaver): ...
+    class ERC20(_AddressHaver):
+        address: ChecksumAddress
 
 
 AnyAddressOrContract = Union[Address, HexAddress, ChecksumAddress, Contract, ERC20]
