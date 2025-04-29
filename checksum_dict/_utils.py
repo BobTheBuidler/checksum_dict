8e285b845d2ba1b630e06f7023245cdd4f2767f5
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-29 05:21:56 -0400

    Update _utils.py

--- checksum_dict/_utils.py
+++ checksum_dict/_utils.py
@@ -4,31 +4,34 @@ The following code was ripped out of eth-brownie on 2022-Aug-06.
 A big thanks to the many maintainers and contributors for their valuable work!
 """
 
-from typing import Final, Union
+from typing import Dict, Final, Union
 
 import cchecksum
 from eth_typing import ChecksumAddress
 
 from checksum_dict import _typing
-from checksum_dict._typing import Contract, ERC20
+
+if TYPE_CHECKING:
+    from brownie import Contract
+    from y import ERC20
 
 
-Contract: Final = _typing.Contract  # type: ignore [misc]
-ERC20: Final = _typing.ERC20  # type: ignore [misc]
+# I do this hacky thing to help out mypyc
+_KNOWN_CHECKSUMMED_TYPES: Final[Dict[type, bool]] = {}
 
 # must not be Final so it can be redefined with lru cache in ypricemagic
 to_checksum_address = cchecksum.to_checksum_address
 
-
-def attempt_checksum(value: Union[str, bytes, Contract]) -> ChecksumAddress:
+def attempt_checksum(value: Union[str, bytes, "Contract", "ERC20"]) -> ChecksumAddress:
     # sourcery skip: merge-duplicate-blocks
     if isinstance(value, str):
         return checksum_or_raise(value)
     elif (valtype := type(value)) is bytes:  # only actual bytes type, mypyc will optimize this
         return checksum_or_raise(value.hex())
-    elif isinstance(valtype, (Contract, ERC20)):
-        # already checksummed
-        return value.address  # type: ignore [union-attr]
+    elif _type_has_checksum_addr(valtype):
+        return value.address
+    elif hasattr(valtype, "address"):
+        return checksum_or_raise(value.address)  # type: ignore [union-attr]
     else:  # other bytes types, mypyc will not optimize this
         return checksum_or_raise(value.hex())
 
@@ -38,3 +41,14 @@ def checksum_or_raise(string: str) -> ChecksumAddress:
         return to_checksum_address(string)
     except ValueError as e:
         raise ValueError(f"'{string}' is not a valid ETH address") from e
+
+
+def _type_has_checksum_addr(typ: Type) -> bool:
+    has_checksum_addr = _KNOWN_CHECKSUMMED_TYPES.get(typ)
+    if has_checksum_addr is None:
+        has_checksum_addr = (
+            valtype.__name__ in {"Contract", "ERC20"}
+            and valtype.__module__.split(".")[0] in {"brownie", "dank_mids", "y"}
+        )
+        _KNOWN_CHECKSUMMED_TYPES[typ] = has_checksum_addr
+    return has_checksum_addr
