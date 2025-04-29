2f1e0370803ee1f19ebcdcb728bc9ae288011e2d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-29 04:32:10 -0400

    fix: handle y.ERC20 type inputs

--- checksum_dict/_typing.py
+++ checksum_dict/_typing.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, Union
+from typing import TYPE_CHECKING, Any, Protocol, Union
 
 from eth_typing import Address, ChecksumAddress, HexAddress  # type: ignore [import-not-found]
 
@@ -6,15 +6,20 @@ from eth_typing import Address, ChecksumAddress, HexAddress  # type: ignore [imp
 # We need to assign mypyc has issues compiling TYPE_CHECKING
 # TODO: PR to mypyc to fix this
 
+class _AddressHaver(Protocol):
+    address: ChecksumAddress
+
+    
 if TYPE_CHECKING:
     import brownie  # type: ignore [import-not-found]
     import y  # type: ignore [import-not-found]
 
     Contract = Union[brownie.Contract, y.Contract]
+    ERC20 = y.ERC20
 
 else:
+    class Contract(_AddressHaver): ...
+    class ERC20(_AddressHaver): ...
 
-    Contract = Any
 
-
-AnyAddressOrContract = Union[Address, HexAddress, ChecksumAddress, Contract]
+AnyAddressOrContract = Union[Address, HexAddress, ChecksumAddress, Contract, ERC20]
