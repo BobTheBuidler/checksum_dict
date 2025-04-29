baaceb9a89bd8b7fbffc376ea2efd680d8ff26b9
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-29 15:06:15 -0400

    fix: client-side C build (#64)

--- build/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-from mypyc.build import mypycify
-
-setup(name='mypyc_output',
-      ext_modules=mypycify(['checksum_dict/default.py', '--strict', '--pretty', '--disable-error-code', 'unused-ignore', '--disable-error-code', 'import-not-found', '--install-types'], opt_level="3", debug_level="1", strict_dunder_typing=True),
-)
--- checksum_dict/_typing.py
+++ checksum_dict/_typing.py
@@ -1,13 +1,13 @@
 from typing import TYPE_CHECKING, Any, Protocol, Union
 
-from eth_typing import Address, ChecksumAddress, HexAddress  # type: ignore [import-not-found]
+from eth_typing import Address, ChecksumAddress, HexAddress
 
 
 # We need to assign mypyc has issues compiling TYPE_CHECKING
 # TODO: PR to mypyc to fix this
 if TYPE_CHECKING:
-    import brownie  # type: ignore [import-not-found]
-    import y  # type: ignore [import-not-found]
+    import brownie
+    import y
 
     Contract = brownie.Contract
     ERC20 = y.ERC20
@@ -21,4 +21,4 @@ else:
         address: ChecksumAddress
 
 
-AnyAddressOrContract = Union[Address, HexAddress, ChecksumAddress, Contract, ERC20]
+AnyAddressOrContract = Union[Address, HexAddress, ChecksumAddress, Contract, ERC20]  # type: ignore [valid-type]
