c55a76cf06cbfb6ad863d4753d57754630519fde
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-04-28 21:20:02 -0400

    feat: build modules as one unit (#52)

--- build/checksum_dict/base.c
+++ build/checksum_dict/base.c
@@ -4,11 +4,11 @@ PyMODINIT_FUNC
 PyInit_base(void)
 {
     PyObject *tmp;
-    if (!(tmp = PyImport_ImportModule("checksum_dict.base__mypyc"))) return NULL;
+    if (!(tmp = PyImport_ImportModule("3fed2dd976ff832859ee__mypyc"))) return NULL;
     PyObject *capsule = PyObject_GetAttrString(tmp, "init_checksum_dict___base");
     Py_DECREF(tmp);
     if (capsule == NULL) return NULL;
-    void *init_func = PyCapsule_GetPointer(capsule, "checksum_dict.base__mypyc.init_checksum_dict___base");
+    void *init_func = PyCapsule_GetPointer(capsule, "3fed2dd976ff832859ee__mypyc.init_checksum_dict___base");
     Py_DECREF(capsule);
     if (!init_func) {
         return NULL;
