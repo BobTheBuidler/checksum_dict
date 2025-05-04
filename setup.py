2feff73e3d9e2f76af44ebc5e7360c12d4bd26cc
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-04 18:27:32 -0400

    chore: drop python3.6

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ setup(
     license="MIT",
     author="BobTheBuidler",
     author_email="bobthebuidlerdefi@gmail.com",
+    python_requires=">=3.7",
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
